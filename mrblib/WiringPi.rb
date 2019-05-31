ec543a1b699f6464e73420a2134896ee012d7c03
ASAHI,Michiharu <m-asahi@aist.go.jp>
2019-05-31 14:35:16 +0900

    enable all GPIO pins.

--- mrblib/WiringPi.rb
+++ mrblib/WiringPi.rb
@@ -60,7 +60,7 @@ module WiringPi
 
   class GPIO
     GPIO_PINS = [
-      0,1,4,7,8,9,10,11,14,15,17,18,21,22,23,24,25 # seemingly random indeed!
+      0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,21,22,23,24,25,26,27,28,29,30,31  # seemingly random indeed!
     ]
 
     PINS = [
